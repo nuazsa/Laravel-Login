7d27c9c8a7b5c6d66ef2c77b13dd2044438a12a6
nuazsa <nurazissaputra@gmail.com>
2024-07-18 22:48:08 +0700

    Authentication system from Breeze

--- routes/web.php
+++ routes/web.php
@@ -1,20 +1,20 @@
 <?php
 
+use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('login', ['title' => 'Login']);
-});
-
-Route::get('/register', function () {
-    return view('register', ['title' => 'Register']);
-});
+// Route::get('/', function () {
+//     return view('welcome');
+// });
 
 Route::get('/dashboard', function () {
-    return view('dashboard', ['title' => 'Dashboard']);
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
+
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
 
-Route::get('/forgot-password', function () {
-    return abort(500);
-    // return view('forgot-password', ['title' => 'Forgot Password']);
-});
+require __DIR__.'/auth.php';
